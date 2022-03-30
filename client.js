c5275f86948ac0816017a280a092c4742c31935f
Madeline Prickette <madelineprickette@gmail.com>
2022-03-30 10:20:32 -0500

    completed a sayHello button and a sayHello function

--- client.js
+++ client.js
@@ -1,7 +1,17 @@
 console.log('js');
 
-$( docuement ).ready( readyNow );
+$( document ).ready( readyNow );
 
 function readyNow(){
     console.log('JQ');
+    $( '#sayHelloButton' ).on( 'click', sayHello );
 }//end readyNow
+
+function sayHello(){
+    //target input element by id
+    //get the value of the text input
+    let userName = $( '#nameInput' ).val();
+    console.log( 'hello', userName );
+    //empty input
+    $( '#nameInput' ).val( '' );//sets value to empty string ''
+}//end sayHello
